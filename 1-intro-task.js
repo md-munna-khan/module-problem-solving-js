2b3e30b63728264e61677699e2b9ad8a220d4978
md.munna <mk8761174@gmail.com>
2024-09-06 00:09:59 +0600

    module task solving

---
+++ 1-intro-task.js
@@ -0,0 +1,82 @@
+/**
+ * Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+Sample Input:
+1000
+700
+
+Sample Output:
+300
+ */
+// output
+// let haveMoney = 800;
+// let orangePrice = 300;
+// let ApplePrice = 200;
+// let FruitsPrice = orangePrice+ApplePrice;//500
+// console.log(FruitsPrice);
+// console.log(haveMoney-FruitsPrice) //300
+
+/**
+ * Task-2
+Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+Input:
+The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+Output:
+Print the result in 2 decimal places.
+
+Sample Input:
+75.25, 65, 80, 35.45, 99.50
+
+Sample Output:
+71.04
+ */
+
+// let Mathematics = 75.25;
+// let Biology = 65;
+// let chemistry =80;
+// let physics =35.45;
+// let bangla =99.50;
+// let totalMarks = Mathematics+Biology+chemistry+physics+bangla;
+// console.log(totalMarks)
+// let devide = 5;
+// let devided = totalMarks/devide;
+// console.log(devided.toFixed(2)) 
+/**
+ * 355.2
+71.03999999999999
+
+355.2               
+71.04
+ */
+/**
+ 
+
+
+ * Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+Sample Input:
+119
+
+Sample Output:
+4
+ */
+let devideNumber =119;
+let remainder = 5;
+let ans = devideNumber%remainder;
+console.log(ans) // ans 4
